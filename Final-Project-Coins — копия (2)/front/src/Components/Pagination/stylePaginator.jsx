db3c90ce5ec74e2ca471f7989f1c786bd885f103
mehrios <66752268+mehrios@users.noreply.github.com>
2023-01-01 21:30:07 +0400

    Delete Final-Project-Coins — копия (2) directory

--- Final-Project-Coins — копия (2)/front/src/Components/Pagination/stylePaginator.jsx
+++
@@ -1,6 +0,0 @@
-import styled from "styled-components";
-export const PaginatorMain = styled.div``;
-export const Page = styled.div`
-  margin-bottom: 20px;
-  height: 400px;
-`;
--- Final-Project-Coins — копия (2)/server/randomString.js
+++
@@ -1,5 +0,0 @@
-function randomString() {
-  return Math.random().toString(36).substring(2) + Math.random().toString(36).substring(2) + Math.random().toString(36).substring(2);
-}
-
-module.exports = randomString;
